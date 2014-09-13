d9c7070f8cc801ce3af2883e7c4989507d36dfe2
Max Mizikar <maxmzkr@gmail.com>
2014-09-12 23:51:56 -0400

    Greeting script that works

--- synergy/synergy_client.sh
+++ synergy/synergy_client.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-ssh -f -N -L localhost:24800:10.0.0.9:24800 max@10.0.0.9 -i /home/max/.ssh/synergy && /usr/bin/synergyc -f localhost
+ssh -f -N -L localhost:24800:10.0.0.9:24800 max@10.0.0.9 -i /home/max/.ssh/synergy
+sudo synergyc localhost
