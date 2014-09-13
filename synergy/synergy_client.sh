ec2cfe3fcec46afb206b4e2efec3c5d958393c46
Max Mizikar <maxmzkr@gmail.com>
2014-09-12 22:30:14 -0400

    Fixed ssh spelling error

--- synergy/synergy_client.sh
+++ synergy/synergy_client.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -f -N -L localhost:24800:10.0.0.9:24800 max@10.0.0.9 -i /home/max/.shh/synergy && /usr/bin/synergyc -f localhost
+ssh -f -N -L localhost:24800:10.0.0.9:24800 max@10.0.0.9 -i /home/max/.ssh/synergy && /usr/bin/synergyc -f localhost
